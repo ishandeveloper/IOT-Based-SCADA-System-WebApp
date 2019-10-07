94bdd42ab6b6b91c2b197807651d1ba3de45f9d4
Ishan Sharma <54989142+ishandeveloper@users.noreply.github.com>
2019-10-08 00:49:43 +0530

    Added HTML Pages

---
+++ stat.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>website</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Advent+Pro">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Alegreya+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amiri">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Averia+Libre">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Encode+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body>
+    <div>
+        <header>
+            <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white transparency border-bottom border-light" id="transmenu">
+                <div class="container"><a class="navbar-brand text-success" href="#header"><i class="fa fa-wrench fa-2x" style="width: 40px;height: 40px;"></i>&nbsp;TINKERING GEEKS</a><button class="navbar-toggler collapsed" data-toggle="collapse" data-target="#navcol-1"><span></span><span></span><span></span></button>
+                    <div
+                        class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="nav navbar-nav ml-auto">
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="index.html">Home</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="untitled.html">Product</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="#">Team</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="#">Contact</a></li>
+                            <li class="nav-item" role="presentation"><a class="nav-link" href="#">FAQ</a></li>
+                            <li class="nav-item" role="presentation" id="login-btn"><a class="nav-link" href="login.php">Login</a></li>
+                        </ul>
+                </div>
+    </div>
+    </nav>
+    <div class="bg-success d-flex align-items-center" style="height: 15vh;background-size: cover;background-color: #4fb4df;background-image: url(&quot;assets/img/background.png&quot;);">
+        <div class="text-center w-100 text-white"></div>
+    </div>
+    </header>
+    </div>
+    <div class="highlight-clean">
+        <div class="container">
+            <div class="intro">
+                <h2 class="text-center">DATA ANALYTICS</h2>
+                <p class="text-center">Here is all the data we've gathered from the sensors placed in your system.</p>
+            </div>
+            <div class="buttons"><a class="btn btn-primary" role="button" href="#">refresh</a></div>
+        </div>
+    </div>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 align-content-center"><img src="assets/img/72001962-stock-vector-humidity-vector-icon.jpg" style="width: 219px;margin-left: 10%;">
+                    <h1 style="margin-left: 0;font-size: 32px;width: 257px;font-family: 'Advent Pro', sans-serif;">Humidity&nbsp;</h1>
+                </div>
+                <div class="col-md-3"><img src="assets/img/download.png" style="width: 219px;margin-left: 10%;padding: 38px;">
+                    <h1 style="margin-left: 0;font-size: 32px;width: 257px;font-family: 'Advent Pro', sans-serif;">Temperature</h1>
+                </div>
+                <div class="col-md-3"><img src="assets/img/images.png" style="width: 219px;margin-left: 6.5%;padding: 51px;">
+                    <h1 style="margin-left: 0;font-size: 32px;width: 257px;font-family: 'Advent Pro', sans-serif;">Pressure</h1>
+                </div>
+                <div class="col-md-3"><img src="assets/img/coolant-level-indicator-liquid-oil-radiator-low-512.png" style="width: 219px;margin-left: 6.5%;padding: 28px;">
+                    <h1 style="margin-left: 0;font-size: 32px;width: 257px;font-family: 'Advent Pro', sans-serif;">Tank Level</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
